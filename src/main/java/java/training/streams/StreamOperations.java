8625a5bb87d2159d75494cbb1f5ebcd27f0c5732
HenyKam <heny.kamoun@gmail.com>
2026-05-04 17:22:13 +0200

    Methodes Tasks added

--- src/main/java/java/training/streams/StreamOperations.java
+++ src/main/java/java/training/streams/StreamOperations.java
@@ -8,13 +8,29 @@ import java.util.stream.Collectors;
 public class StreamOperations {
     static void main(String[] args) {}
 
-    public List<String> listBooksAuthorsFullNames(List<Book> books) {
-        return null;
-    }
-
     public Set<Book.Genre> listBooksGenres(List<Book> books) {
         return books.stream()
                 .map(Book::genre)
                 .collect(Collectors.toSet());
+    }
+
+    public List<String> ListBooksTitleOfGenreRomance(List<Book> books) {
+        return null;
+    }
+
+    public int sumPageCountOfBooksPublishedIn2025(List<Book> books) {
+        return 0;
+    }
+
+    public String getTitleOfOldestBook(List<Book> books) {
+        return null;
+    }
+
+    public String getAuthorAgeOfNewestBook(List<Book> books) {
+        return null;
+    }
+
+    public List<String> listBooksAuthorsFullNames(List<Book> books) {
+        return null;
     }
 }
