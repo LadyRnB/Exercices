ecde66b4f65b3b61588d2376df245cb3e8fd4d98
HenyKam <heny.kamoun@gmail.com>
2026-05-05 23:41:50 +0200

    streamOperations resolved

--- src/main/java/java/training/streams/StreamOperations.java
+++ src/main/java/java/training/streams/StreamOperations.java
@@ -1,6 +1,8 @@
 package java.training.streams;
 
+import java.time.LocalDate;
 import java.training.Book;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,27 +12,52 @@ public class StreamOperations {
 
     public Set<Book.Genre> listBooksGenres(List<Book> books) {
         return books.stream()
-                .map(Book::genre)
-                .collect(Collectors.toSet());
+                    .map(Book::genre)
+                    .collect(Collectors.toSet());
     }
 
     public List<String> ListBooksTitleOfGenreRomance(List<Book> books) {
-        return null;
+        return books.stream()
+                    .filter(book -> book.genre().equals(Book.Genre.ROMANCE))
+                    .map(Book::title)
+                    .toList();
     }
 
     public int sumPageCountOfBooksPublishedIn2025(List<Book> books) {
-        return 0;
+        return books.stream()
+                    .filter(book -> book.publicationDate().getYear() == 2025)
+                    .collect((Collectors.summingInt(Book::pageCount)));
+//        return books.stream()
+//                .filter(book -> book.publicationDate().getYear() == 2025)
+//                .mapToInt(Book::pageCount)
+//                .sum();
     }
 
     public String getTitleOfOldestBook(List<Book> books) {
-        return null;
+        return books.stream()
+                    .min(Comparator.comparing(Book::publicationDate))
+                    .map(Book::title)
+                    .orElse(null);
     }
 
-    public String getAuthorAgeOfNewestBook(List<Book> books) {
-        return null;
+    public String getIsbnOfNewestBook(List<Book> books) {
+        return books.stream()
+                    .max(Comparator.comparing(Book::publicationDate))
+                    .map(Book::isbn)
+                    .orElse(null);
     }
 
     public List<String> listBooksAuthorsFullNames(List<Book> books) {
-        return null;
+        return books.stream()
+                    .flatMap(book -> book.authors().stream())
+                    .map(author -> author.firstName() + " " + author.lastName())
+                    .toList();
+    }
+
+    public int sumAgesOfAuthors(List<Book> books) {
+        return books.stream()
+                    .flatMap(book  -> book.authors().stream())
+                    .mapToInt( author -> LocalDate.now().getYear() - author.birthDate().getYear())
+                    .sum();
     }
 }
