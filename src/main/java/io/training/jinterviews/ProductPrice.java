7c7f2362bd7cae17d194198b952780094944358b
Lady R'n'B <rahma.barrak.k@gmail.com>
2026-05-06 17:45:04 +0200

    use enum

--- src/main/java/io/training/jinterviews/ProductPrice.java
+++ src/main/java/io/training/jinterviews/ProductPrice.java
@@ -2,22 +2,23 @@ package io.training.jinterviews;
 
 public class ProductPrice {
 
-    public final double GOLD_RABATT = 0.80;
-    public final double SILVER_RABATT = 0.90;
-    public final double EXPRESSTAX = 10.0;
+    public static final double GOLD_RABATT = 0.80;
+    public static final double SILVER_RABATT = 0.90;
+    public static final double EXPRESSTAX = 10.0;
 
-    public double calculatePrice(double basePrice, String customerType, boolean isExpress) {
-        double price = 0;
-        switch (customerType) {
-            case "GOLD":
-                price = basePrice * GOLD_RABATT;
-                break;
-            case "SILVER":
-                price = basePrice * SILVER_RABATT;
-                break;
-            default:
-                price = basePrice;
-        }
+    public enum CustomerType {
+        GOLD,
+        SILVER,
+        NORMAL
+    }
+
+    public double calculatePrice(double basePrice, CustomerType customerType, boolean isExpress) {
+         double price = switch (customerType){
+            case GOLD -> basePrice * GOLD_RABATT;
+            case SILVER -> basePrice * SILVER_RABATT;
+            default -> basePrice;
+        };
+
         if (isExpress) {
             price += EXPRESSTAX;
         }
--- src/test/java/io/training/jinterviews/ProductPriceTest.java
+++ src/test/java/io/training/jinterviews/ProductPriceTest.java
@@ -2,6 +2,7 @@ package io.training.jinterviews;
 
 import org.junit.jupiter.api.Test;
 
+import static io.training.jinterviews.ProductPrice.CustomerType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -10,19 +11,17 @@ public class ProductPriceTest {
 
     @Test
     public void testCalculatePrice() throws Exception {//Act
-        assertEquals(80.0, productPrice.calculatePrice(100.0, "GOLD", false));
+        assertEquals(80.0, productPrice.calculatePrice(100.0, GOLD, false));
 
-        assertEquals(90.0, productPrice.calculatePrice(100.0, "GOLD", true));
+        assertEquals(90.0, productPrice.calculatePrice(100.0, GOLD, true));
 
-        assertEquals(90.0, productPrice.calculatePrice(100.0, "SILVER", false));
+        assertEquals(90.0, productPrice.calculatePrice(100.0, SILVER, false));
 
-        assertEquals(100.0, productPrice.calculatePrice(100.0, "SILVER", true));
+        assertEquals(100.0, productPrice.calculatePrice(100.0, SILVER, true));
 
-        assertEquals(100.0, productPrice.calculatePrice(100.0, "", false));
+        assertEquals(100.0, productPrice.calculatePrice(100.0, NORMAL, false));
 
-        assertEquals(110.0, productPrice.calculatePrice(100.0, "", true));
-
-
+        assertEquals(110.0, productPrice.calculatePrice(100.0, NORMAL, true));
 
     }
 }
