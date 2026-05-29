446f02337245eb9ad268d8f9f694b2799760790f
HenyKam <heny.kamoun@gmail.com>
2026-05-29 10:05:04 +0200

    first optional challenges

---
+++ src/main/java/java/training/optional/OptionalOperations.java
@@ -0,0 +1,27 @@
+package java.training.optional;
+
+import java.training.Book;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalOperations {
+
+    public String getBookTitleElseUndifined(Optional<Book> bookOptional) {
+        return null;
+    }
+
+    public Optional<Integer> getBookPublicationYear(Optional<Book> bookOptional) {
+        return null;
+    }
+
+    public void printAuthorsNames(Optional<Book> bookOptional) {
+    }
+
+    public String getIsbnElseRuntimeException(Optional<Book> bookOptional) {
+        return null;
+    }
+
+    public List<String> getListOfAuthorsNationalities(Optional<Book> bookOptional) {
+        return null;
+    }
+}
